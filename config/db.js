fd1ba87b96fff074ecb363194789551a8bd51073
neerap-india <tony.dev@changeartwork.com>
2022-12-09 18:31:17 +0530

    changed localhost to ip

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/changeartworkdb', {
+mongoose.connect('mongodb://127.0.0.1:27017/changeartworkdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
