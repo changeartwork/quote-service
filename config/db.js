97a5a15afbfd097a339870b1696def27876fa894
neerap-india <tony.dev@changeartwork.com>
2022-12-09 17:42:06 +0530

    changed mongo to local

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://admin:admin@cluster.qctktes.mongodb.net/changeartworkdb', {
+mongoose.connect('mongodb://localhost:27017/changeartworkdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
